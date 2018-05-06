fed9bd4c732bdb7deee8e54db96546be8c56e7f1
M. Forrest Badgley <forrestbadgley@Ms-MacBook-Pro.attlocal.net>
2018-05-06 15:38:41 -0500

    updates

--- scrape_mars_two.py
+++ scrape_mars_two.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 from splinter import Browser
 import requests
@@ -10,35 +9,28 @@ def init_browser():
         browser = Browser('chrome', headless=False)
 
 def scrape():
-   
     browser = init_browser()
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     response = requests.get(url)
     time.sleep(1)
     soup = BeautifulSoup(response.text, 'lxml')
-    #grabbling the 'slide' class element from the url
-    results = soup.find_all(class_="slide")
-    #creating a list to hold scraped data
-    news_data=[]
-    for result in results:
-    # Error handling
-        try: #loop thru and get the text within these classes, replace \n with blank space
-            news_p = result.find(class_="rollover_description_inner").text.replace('\n', '')
-            news_title = result.find(class_="content_title").text.replace('\n', '')
-           
-            post = {"news_title": news_title,
+    #create an empty dictionary to store values
+    news_data={}
+    #find the appropriate div class containing target data to scrape, scrape text and assign to variables
+    news_p = soup.find_all('div',class_="rollover_description_inner")[0].text.strip()
+    news_title = soup.find_all('div',class_="content_title")[0].text.strip()
+    #put the variables into a dictionary
+    post = {"news_title": news_title,
                     "news_p":news_p}
-
-            news_data.append(post)
-            print(post)
-          
-        except Exception as e:
-               print(e)
+    print(post)
+    #update the main dictionary with the new dictionary 
+    news_data.update(post)
     browser = Browser('chrome', headless=False)
+    #new URL to scrape 
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url)
     time.sleep(1)
-    #use splinter to click the "Full Image" button
+    #use splinter to click on button 
     browser.click_link_by_partial_text('FULL IMAGE')
     time.sleep(1)
     #HTML Object 
@@ -51,8 +43,10 @@ def scrape():
     url = results['src']
     #attach the path to the main site link, this is the full image link
     featured_image_url = 'https://www.jpl.nasa.gov'+url
+    #store the full image link in a dictionary
     post_two = {'featured_image':featured_image_url}
-    news_data.append(post_two)
+    #update the main dictionary with the new dictionary data
+    news_data.update(post_two)
     print(post_two)
     #visit the mars twitter page to get the Weather
     url = 'https://twitter.com/marswxreport?lang=en'
@@ -63,29 +57,39 @@ def scrape():
     soup = BeautifulSoup(response.text, 'html.parser')
     #get the text from the first p tag with appropriate class (from inspecting the site)
     mars_weather = soup.find('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").text
+    #store the weather tweet in a dictionary
     post_three = {'mars_weather':mars_weather}
+    #update the maind dictionary with the new dictionary data
+    news_data.update(post_three)
     print(post_three)
-    news_data.append(post_three)
     browser = Browser('chrome', headless=False)
-    #visit the mars space facts site
+    #new URL to be scraped
     url = 'https://space-facts.com/mars/'
     #read the table, put into list variable
     tables = pd.read_html(url)
     #convert the list to a dataframe
     mars_df =tables[0]
-    #put column headers on 
-    mars_df.columns = ["Characteristic", "Value"]
-    #convert the datframe to dictionary, using 'records' orientation, this does not neeed to be, nor should be, appended to news_data, as will create a list of a dictionary within the list, and not be able to be inserted to mongodb
-    mars_dict=mars_df.to_dict('records')
-    print(mars_dict)
-    #Visit the site to get images of Mars Hemispheres
+    #set column names 
+    mars_df.columns = ["Characteristics","Values"]
+    #set index to Characteristics
+    mars_df = mars_df.set_index(["Characteristics"])
+    #make it an html table
+    mars_df.to_html("mars_data.html")
+    #put the whole table into a dictionary item
+    table_post={"mars_table":mars_df}
+    #update the main dictionary with the table dictionary
+    news_data.update(table_post) 
+    print(table_post)
+    browser = Browser('chrome', headless=False)
+    #new URL to be scraped
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
     time.sleep(1)
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
     results = soup.find_all(class_='item')
-  
+    #an empty array to store dictionary
+    image_data=[]
     #loop through the item class
     for result in results:
         #find the first a tag
@@ -106,16 +110,17 @@ def scrape():
         soup = BeautifulSoup(html, 'html.parser')
         #find all elemenst with class 'downloads', assign results to variable list 'infos'
         infos = soup.find_all(class_='downloads')
-        #loop thru infos, pull out links to images, assign with title to dictionary post, and then append to list
-        #mars_images
+        
         for info in infos:
             link_two=info.find('a')
             img_url=link_two['href']
             post_four={'img_url':img_url, 'title':title}
-            news_data.append(post_four)
-            print(post_four)
+            image_data.append(post_four)
+
+     #update main dictionary with new data
+    post_five={"image_data":image_data}
+    print(post_five)
    
-   #return your data, so it can be accessed by flask app (where the insertion into mongodb will occur)
-    return news_data+mars_dict
-    
+    news_data.update(post_five)
+    return news_data
             
